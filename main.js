212585fb4835d21828ccef38efcefbf6f59b94f7
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-07-03 07:52:22 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 camera= document.getElementById("Camera");
 Webcam.attach(camera);
 Webcam.set({
-    width:700,
-    height:700,
+    height:450,
+    width:500,
     image_format:'png',
     png_quality:90
 
 });
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+
+    });
+}
+console.log('ml5 version:',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/zYuTbZ1v5/model.json',modelLoaded);
+function modelLoaded(){
+    console.log('Model Loaded!');
+    
+}
