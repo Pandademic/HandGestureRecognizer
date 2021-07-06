c8dddcd66060d2fc6378a0289508afba07111c6a
Pandademic <74566464+Pandademic@users.noreply.github.com>
2021-07-06 10:21:45 -0400

    V4 possibly final
    KNOWN BUG:
    1.V for victory be shown as Thumbs up sometimes

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+var speak_data_1;
+var thumbs_up="&#128077;";
+var thumbs_down="&#128078;";
+var VforVictory="&#128406;";
+var img;
 camera= document.getElementById("Camera");
 Webcam.attach(camera);
 Webcam.set({
@@ -14,8 +19,40 @@ function take_snapshot(){
     });
 }
 console.log('ml5 version:',ml5.version);
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/zYuTbZ1v5/model.json',modelLoaded);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/hkJmYpDSK/model.json',modelLoaded);
 function modelLoaded(){
     console.log('Model Loaded!');
     
+}
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data_1="Your gesture is a  "+prediction_1;
+        var utterThis=new SpeechSynthesisUtterance(speak_data_1);
+    synth.speak(utterThis);
+  
+  }
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+      console.error(error);
+
+    }
+    else{
+       console.log(results);
+       document.getElementById("name_of_gesture").innerHTML=results[0].label;
+       prediction_1=results[0].label;
+       speak();
+       if(results[0].label=="thumbs Up"){
+           document.getElementById("emoji_of_gesture").innerHTML=thumbs_up
+       }
+       if(results[0].label=="Thumbs Down"){ 
+        document.getElementById("emoji_of_gesture").innerHTML="&#128078;";
+       }
+       if(results[0].label=="V for Victory"){
+        document.getElementById("emoji_of_gesture").innerHTML="&#128406;";
+       }
+    }
 }
